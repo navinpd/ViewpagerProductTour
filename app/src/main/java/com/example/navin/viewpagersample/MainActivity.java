daaec30a66b058d7e31148f84ba9341e2cd2e18f
Navin <navin.pd@gmail.com>
2015-08-11 00:12:07 +0800

    First commit

---
+++ app/src/main/java/com/example/navin/viewpagersample/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.navin.viewpagersample;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Parcelable;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends Activity {
+    LinearLayout buttonHolder;
+    int Count = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Count = 2;
+
+        ViewPager vp = (ViewPager) findViewById(R.id.viewPager);
+        buttonHolder = (LinearLayout) findViewById(R.id.dots_holder);
+
+        addChild();
+
+        MyPagerAdapter adapter = new MyPagerAdapter(this);
+        vp.setAdapter(adapter);
+
+        final TextView tvHeader = (TextView) findViewById(R.id.textViewHeader);
+        tvHeader.setText("Page 1");
+
+        vp.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            @Override
+            public void onPageScrollStateChanged(int arg0) {
+            }
+
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
+            }
+
+            @Override
+            public void onPageSelected(int arg0) {
+                tvHeader.setText("Page " + (arg0 + 1));
+
+                for (int i = 0; i < Count; i++) {
+                    ImageView circle = (ImageView) buttonHolder.getChildAt(i);
+                    if (i == arg0) {
+                        circle.setColorFilter(getResources().getColor(R.color.text_selected));
+                    } else {
+                        circle.setColorFilter(getResources().getColor(android.R.color.transparent));
+                    }
+                }
+            }
+
+        });
+
+    }
+
+    private void addChild() {
+        for (int i = 0; i < Count; i++) {
+            ImageView iv = new ImageView(this);
+            iv.setImageResource(R.drawable.ic_swipe_indicator_white_18dp);
+            buttonHolder.addView(iv);
+        }
+
+        ImageView view1 = (ImageView) buttonHolder.getChildAt(0);
+        view1.setColorFilter(getResources().getColor(R.color.text_selected));
+    }
+
+}
---
+++ app/src/main/java/com/example/navin/viewpagersample/MyPagerAdapter.java
@@ -0,0 +1,68 @@
+package com.example.navin.viewpagersample;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Navin on 10/8/15.
+ */
+
+public class MyPagerAdapter extends PagerAdapter {
+    private ArrayList<LinearLayout> arry;
+    Context context;
+
+    public MyPagerAdapter(Context context) {
+        this.context = context;
+        arry = new ArrayList<LinearLayout>();
+        arry.add(new LinearLayout(context));
+        arry.add(new LinearLayout(context));
+    }
+
+    @Override
+    public void destroyItem(ViewGroup view, int arg1, Object object) {
+        ((ViewPager) view).removeView((LinearLayout) object);
+    }
+
+    @Override
+    public int getCount() {
+        return arry.size();
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup view, int position) {
+
+        View v = new View(context.getApplicationContext());
+
+        final LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        switch (position) {
+            case 0:
+                v = inflater.inflate(R.layout.jaw, (ViewGroup) null, false);
+                break;
+            case 1:
+                v = inflater.inflate(R.layout.hello, null, false);
+                break;
+            default:
+
+
+                break;
+        }
+        ((ViewPager) view).addView(v, 0);
+
+        return v;
+    }
+
+    @Override
+    public boolean isViewFromObject(View arg0, Object arg1) {
+        return arg0 == arg1;
+    }
+
+}
